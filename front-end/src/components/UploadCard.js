3c308843a6f0ddb7dcd3e6820cc7003947c80f8f
0x30c4 <abdullahsanaf@gmail.com>
2021-11-15 22:41:08 +0600

    All Good!

--- front-end/src/components/UploadCard.js
+++ front-end/src/components/UploadCard.js
@@ -16,10 +16,12 @@ export default function UploadCard( { onChangeHandler, onClickHandler, loaded} )
             </CardHeader>
             <CardBody>
                 <CardText>
-                    Fast and Free OCR service.
-                    Extract text from any image with in few seconds.
-                    Simple Optical Character Recognition service.
-                    Maximum File upload size is 8MB.
+                    Fast and Free OCR service. Extract text from <br/>
+                    any image with in few seconds. Simple Optical <br/>
+                    Character Recognition service. Maximum File <br/>
+                    upload size is 8MB. <br/>
+                    The <a href="https://github.com/0x30c4/fastOCR/">GitHub Repo</a> link. <br/>
+                    API <a href="https://ocr.0x30c4.dev/api/docs">Documentation</a> link.
                 </CardText>
                 <FormGroup>
                     <input type="file" className="form-control" onChange={onChangeHandler}/>
